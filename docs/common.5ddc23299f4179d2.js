d1b5a3498de8fee6513e8631d98119cecb3fc7a8
Dmitry Shapran <djmitrofanov@gmail.com>
2024-02-05 01:12:51 +0300

    add docs

---
+++ docs/common.5ddc23299f4179d2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkTea=self.webpackChunkTea||[]).push([[592],{264:(u,t,r)=>{r.d(t,{Q:()=>c});var a=r(8256);let c=(()=>{class e{constructor(){this.teaTitle=""}}return e.\u0275fac=function(o){return new(o||e)},e.\u0275prov=a.Yz7({token:e,factory:e.\u0275fac,providedIn:"root"}),e})()}}]);
